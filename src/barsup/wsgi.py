e43f58e648647fcbb98a62b6ac66dcae51943897
Telepenin Nikolay <telepenin@bars-open.ru>
2014-12-09 15:26:57 +0300

    Временная возможность загрузки страниц через url - http://localhost:8000/barsup/index.html

--- src/barsup/wsgi.py
+++ src/barsup/wsgi.py
@@ -15,27 +15,26 @@ def api(request):
 
 # Приложение, раздающее статику
 static_serving_app = DirectoryApp(
-    path.join(environ['BUP_PATH'], 'static', 'barsup'),
-    index_page='index.html',
-    hide_index_with_redirect=True
+    path.join(environ['BUP_PATH'], 'static'),
+    index_page='barsup/',
+    hide_index_with_redirect=False
 )
 
 
 @wsgify.middleware
 def serve_static(request, app):
-    # url = request.path
-    # if url == '/' or (
-    #     url.startswith('/index') or
-    #     url.startswith('/static/')
-    # ):
-    #     return static_serving_app
-    # else:
-    #     return app
-    res = request.call_application(static_serving_app)
-    if res[0] != '200 OK':
-        return app
-    else:
+    url = request.path
+    if url == '/' or (
+        url.startswith('/barsup')
+    ):
         return static_serving_app
+    else:
+        return app
+    # res = request.call_application(static_serving_app)
+    # if res[0] != '200 OK':
+    #     return app
+    # else:
+    #     return static_serving_app
 
 
 application = serve_static(api)
