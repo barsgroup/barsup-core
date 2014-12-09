d3a5d6b131aff6ddc4035a66a2ce361a278b33bb
Пирогов Алексей <none@none>
2014-12-09 16:34:22 +0300

    Debug Mode

--- src/barsup/wsgi.py
+++ src/barsup/wsgi.py
@@ -1,16 +1,20 @@
 # coding:utf-8
 
 from os import environ, path
+from sys import stderr, exc_info
+from datetime import datetime
+import traceback
 
 from webob import Response
 from webob.dec import wsgify
-from webob.exc import HTTPMovedPermanently
+from webob.exc import HTTPMovedPermanently, HTTPInternalServerError
 from webob.static import DirectoryApp
 
 
 @wsgify
 def api(request):
     """WSGI-приложение"""
+    raise ValueError('dasdasd')
     return Response('Hello!')
 
 
@@ -32,4 +36,29 @@ def serve_static(request, app, prefix):
         return app
 
 
-application = serve_static(api, prefix='/barsup')
+@wsgify.middleware
+def catch_errors(request, app, debug=False):
+    try:
+        return request.call_application(app)
+    except Exception:
+        trace = ''.join(traceback.format_exception(*exc_info(), limit=20))
+        stderr.write('%s\n%s\n' % (
+            datetime.now().strftime('%Y.%m.%d/%H:%M:%S'),
+            trace
+        ))
+        if debug:
+            params = {'body_template': '<pre>%s</pre>' % trace}
+        else:
+            params = {}
+        raise HTTPInternalServerError(**params)
+
+
+application = serve_static(
+    catch_errors(
+        api,
+        debug=True
+    ),
+    prefix='/barsup')
+
+
+__all__ = (application, api, catch_errors, serve_static)
