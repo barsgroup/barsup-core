bca8ed0dc1b7bf408f5210376ec4bdcee0a7b7fa
Пирогов Алексей <none@none>
2014-12-22 14:56:48 +0300

    initware для контроллера прав

--- src/barsup/auth/__init__.py
+++ src/barsup/auth/__init__.py
@@ -1,1 +1,10 @@
 # coding: utf-8
+
+AVAILABLE_ACTIONS = []
+
+
+def list_actions(cont, api):
+    """
+    initware, получающее список экшнов для подсистемы проверки прав
+    """
+    AVAILABLE_ACTIONS.extend(api)
