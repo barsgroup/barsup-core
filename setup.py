ce8535b51dbe9146663efaf62d773d368c7a75d3
Telepenin Nikolay <telepenin@bars-open.ru>
2014-12-25 19:05:59 +0300

    При автодеплое тагов необходимо указывать all_branches: true

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="barsup-core",
-    version="0.1.4",
+    version="0.1.5",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Telepenin Nikolay, Aleksey Pirogov",
