88de71f4a654f5596fcddbd3f2c4e07e40cc336b
Telepenin Nikolay <telepenin@bars-open.ru>
2014-12-25 11:15:00 +0300

    Конфигурация для travis.ci

--- src/barsup/util.py
+++ src/barsup/util.py
@@ -10,8 +10,6 @@ from yadic.util import merge
 def serialize_to_json(obj):
     if isinstance(obj, (map, set, GeneratorType)):
         return tuple(obj)
-    elif isinstance(obj, date):
-        return date.strftime(obj, '%m/%d/%Y')
     else:
         raise TypeError('Type "{0}" not supported'.format(obj))
 
