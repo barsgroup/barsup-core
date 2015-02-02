b35e868142e1cce388a080336a3ccdc345eef8f7
Пирогов Алексей <none@none>
2015-02-02 14:46:07 +0300

    iteration for submodule actions

--- src/barsup/auth/__init__.py
+++ src/barsup/auth/__init__.py
@@ -7,4 +7,9 @@ def list_actions(cont, api):
     """
     initware, получающее список экшнов для подсистемы проверки прав
     """
-    AVAILABLE_ACTIONS.extend(api)
+    from barsup.core import iter_apis
+
+    for path, sub_api in iter_apis(api):
+        path = '/'.join(path + ('%s',))
+        for ctl, action in sub_api:
+            AVAILABLE_ACTIONS.append((path % ctl, action))
